6038612b6125e93fbf7bfcf111139f3b876a2f5e
mobyfab <mobyfab@free.fr>
2012-09-11 18:30:40 +0200

    Small modifications

--- STM32F4-LCD-GWEN/include/gui.h
+++ STM32F4-LCD-GWEN/include/gui.h
@@ -20,9 +20,16 @@ public:
 
 	void Button1_pressed(Controls::Base* control) {
 		chprintf((BaseSequentialStream *)&SD2, "Button 1 Pressed\n");
+		Pgb1_changed(this);
 	}
 	void Button2_pressed(Controls::Base* control) {
 		chprintf((BaseSequentialStream *)&SD2, "Button 2 Pressed\n");
+		Pgb1_changed(this);
+	}
+	void Pgb1_changed(Gwen::Controls::Base* control) {
+		std::string tmp("PGB1 val changed: ");
+		tmp += Gwen::Utility::ToString((unsigned int)m_pgb1->GetValue()*100);
+		chprintf((BaseSequentialStream *)&SD2, tmp.c_str());
 	}
 
 private:
