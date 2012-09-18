cd710e59c901362580d9ca5b97c4ac6adad93fca
mobyfab <mobyfab@free.fr>
2012-09-18 22:56:49 +0200

    Increased systick switches back to Os instead of O2

--- STM32F4-LCD-C++/chconf.h
+++ STM32F4-LCD-C++/chconf.h
@@ -45,7 +45,7 @@
  *          setting also defines the system tick time unit.
  */
 #if !defined(CH_FREQUENCY) || defined(__DOXYGEN__)
-#define CH_FREQUENCY                    1000
+#define CH_FREQUENCY                    200000
 #endif
 
 /**
