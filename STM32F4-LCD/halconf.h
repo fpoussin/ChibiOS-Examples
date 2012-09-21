9135948c3be64a1b542bb1d6dc04928db4ddac84
mobyfab <mobyfab@free.fr>
2012-09-21 11:14:03 +0200

    Using CCM (Core Coupled Memory) to store stack variables for better performance.

--- STM32F4-LCD/halconf.h
+++ STM32F4-LCD/halconf.h
@@ -341,6 +341,26 @@
 /* GDISP driver related settings.                                              */
 /*===========================================================================*/
 
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 272
+
+#define LCD_USE_FSMC
+//#define LCD_USE_GPIO
+
+#define LCD_USE_DMA
+#define LCD_DMA_STREAM STM32_DMA2_STREAM6
+
+#if defined(LCD_USE_GPIO)
+
+    #define LCD_CMD_PORT GPIOC
+    #define LCD_DATA_PORT GPIOD
+
+    #define LCD_CS 0
+    #define LCD_RS 1
+    #define LCD_WR 2
+    #define LCD_RD 3
+#endif
+
 /**
   * @brief   Enables the Touchpad subsystem.
   */
