dd665282d10fbe5f4fc2a003b58e8bcdfa0d2801
mobyfab <mobyfab@free.fr>
2012-10-01 20:47:16 +0200

    updated examples to works with GFX 1.1

--- STM32F4-LCD-C++/halconf.h
+++ STM32F4-LCD-C++/halconf.h
@@ -344,21 +344,21 @@
 #define SCREEN_WIDTH 480
 #define SCREEN_HEIGHT 272
 
-#define LCD_USE_FSMC
-//#define LCD_USE_GPIO
+#define GDISP_USE_FSMC
+//#define GDISP_USE_GPIO
 
-#define LCD_USE_DMA
-#define LCD_DMA_STREAM STM32_DMA2_STREAM6
+#define GDISP_USE_DMA
+#define GDISP_DMA_STREAM STM32_DMA2_STREAM6
 
-#if defined(LCD_USE_GPIO)
+#if defined(GDISP_USE_GPIO)
 
-    #define LCD_CMD_PORT GPIOC
-    #define LCD_DATA_PORT GPIOD
+    #define GDISP_CMD_PORT GPIOC
+    #define GDISP_DATA_PORT GPIOD
 
-    #define LCD_CS 0
-    #define LCD_RS 1
-    #define LCD_WR 2
-    #define LCD_RD 3
+    #define GDISP_CS 0
+    #define GDISP_RS 1
+    #define GDISP_WR 2
+    #define GDISP_RD 3
 #endif
 /**
   * @brief   Enables the Touchpad subsystem.
