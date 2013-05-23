004e4596c0d29bdff41fb39286de1b0cd4b6556f
mobyfab <mobyfab@gmail.com>
2013-05-23 22:52:52 +0200

    More testing.

--- STM32F4-LCD-DSP/main.c
+++ STM32F4-LCD-DSP/main.c
@@ -29,7 +29,7 @@
 #include "arm_math.h"
 
 #define TEST_LENGTH_SAMPLES 2048
-#define FFT_SIZE 1024
+#define FFT_SIZE 256
 
 /* ------------------------------------------------------------------
 * Global variables for FFT Bin Example
@@ -40,7 +40,7 @@ const uint32_t doBitReverse = 1;
 /* -------------------------------------------------------------------
 * External Input and Output buffer Declarations for FFT Bin Example
 * ------------------------------------------------------------------- */
-extern float32_t testInput_f32_10khz[TEST_LENGTH_SAMPLES];
+extern const float32_t testInput_f32_10khz[TEST_LENGTH_SAMPLES];
 static float32_t testOutput[FFT_SIZE];
 
 /* Reference index at which max energy of bin occurs in the test samples
@@ -49,6 +49,8 @@ const uint32_t refIndex = 213;
 uint32_t testIndex = 0;
 
 int uitoa(unsigned int value, char * buf, int max);
+
+float32_t input_tmp[TEST_LENGTH_SAMPLES];
 
 static WORKING_AREA(waThread2, 16384);
 __attribute__ ((__noreturn__))
@@ -72,8 +74,11 @@ static msg_t Thread2(void *arg)  {
 	const font_t font1 = gdispOpenFont("UI2 Double");
 	const char *msg = "ChibiOS/GFX DSP Demo";
 
-	const char *dsp_success = "FFT SUCCESS";
 	const char *dsp_fail = "FFT FAILED";
+
+	uint32_t cnt;
+	char time_str[25];
+	uint32_t us;
 
 	arm_status status;
 	arm_cfft_radix4_instance_f32 S;
@@ -82,20 +87,27 @@ static msg_t Thread2(void *arg)  {
 	while (TRUE) {
 
 		status = ARM_MATH_SUCCESS;
-		gdispClear(Black);
+		gdispClear(Blue);
+		memcpy(input_tmp, testInput_f32_10khz, TEST_LENGTH_SAMPLES);
+		memset(testOutput, 0, FFT_SIZE);
 
 		/* Initialize the CFFT/CIFFT module */
 		status = arm_cfft_radix4_init_f32(&S, FFT_SIZE, ifftFlag, doBitReverse);
 
+		cnt = halGetCounterValue();
 		/* Process the data through the CFFT/CIFFT module */
-		arm_cfft_radix4_f32(&S, testInput_f32_10khz);
+		arm_cfft_radix4_f32(&S, input_tmp);
 
 		/* Process the data through the Complex Magnitude Module for
 		calculating the magnitude at each bin */
-		arm_cmplx_mag_f32(testInput_f32_10khz, testOutput, FFT_SIZE);
+		arm_cmplx_mag_f32(input_tmp, testOutput, FFT_SIZE);
 
+		cnt = halGetCounterValue() - cnt;
+		memset (time_str, 0, sizeof(time_str));
+		us = cnt / 168;
+		uitoa(us, time_str, sizeof(time_str));
 		/* Calculates maxValue and returns corresponding BIN value */
-		arm_max_f32(testOutput, FFT_SIZE, &maxValue, &testIndex);
+		//arm_max_f32(testOutput, FFT_SIZE, &maxValue, &testIndex);
 
 		/* We verify the result is what it should be */
 		if(testIndex !=  refIndex)
@@ -104,19 +116,37 @@ static msg_t Thread2(void *arg)  {
 		}
 
 		/* Print success/fail string on LCD */
-		if( status != ARM_MATH_SUCCESS)
+/*		if( status != ARM_MATH_SUCCESS)
 		{
-			gdispDrawString(width-gdispGetStringWidth(dsp_fail, font1)-3, height/2, dsp_fail, font1, White);
+			gdispDrawString((width/2)-gdispGetStringWidth(dsp_fail, font1)/2, height/2, dsp_fail, font1, White);
 		}
 		else
 		{
-			gdispDrawString(width-gdispGetStringWidth(dsp_success, font1)-3, height/2, dsp_success, font1, White);
-		}
+	*/
+			int i;
+			for (i=0 ; i < FFT_SIZE && i < width ; i++) {
 
-		gdispDrawString(width-gdispGetStringWidth(msg, font1)-3, height-24, msg, font1, White);
+				gdispFillArea(i*2, 0, 2, testOutput[i]*3, White);
+			}
+		//}
+		gdispDrawString(width-gdispGetStringWidth(time_str, font1)-3, height-24, time_str, font1, Grey);
 
 		chThdSleepMilliseconds(2000);
 	}
+}
+
+static WORKING_AREA(waThread1, 128);
+__attribute__ ((__noreturn__))
+static msg_t Thread1(void *arg) {
+
+  (void)arg;
+  chRegSetThreadName("blinker");
+  while (TRUE) {
+    palSetPad(GPIOD, GPIOD_LED3);       /* Orange.  */
+    chThdSleepMilliseconds(500);
+    palClearPad(GPIOD, GPIOD_LED3);     /* Orange.  */
+    chThdSleepMilliseconds(500);
+  }
 }
 
 /*
@@ -138,7 +168,7 @@ int main(void) {
   /*
    * Creates the example thread.
    */
-//  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
   chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
   while (TRUE) {
     chThdSleepMilliseconds(500);
