2957f3d30ee626c8144ebed283ec0590e3734e3c
mobyfab <mobyfab@free.fr>
2013-06-05 19:30:22 +0200

    Adding QT creator files.

--- STM32F4-LCD/main.c
+++ STM32F4-LCD/main.c
@@ -24,51 +24,50 @@
 #include "gfx.h"
 #include "stdlib.h"
 #include "string.h"
-#include "test.h"
 #include "math.h"
 
 int uitoa(unsigned int value, char * buf, int max) {
-	int n = 0;
-	int i = 0;
-	unsigned int tmp = 0;
+  int n = 0;
+  int i = 0;
+  unsigned int tmp = 0;
 
-	if (NULL == buf) {
-		return -3;
-	}
+  if (NULL == buf) {
+    return -3;
+  }
 
-	if (2 > max) {
-		return -4;
-	}
+  if (2 > max) {
+    return -4;
+  }
 
-	i=1;
-	tmp = value;
-	for (;;) {
-		tmp /= 10;
-		if (0 >= tmp) {
-			break;
-		}
-		i++;
-	}
-	if (i >= max) {
-		buf[0] = '?';
-		buf[1] = 0x0;
-		return 2;
-	}
+  i=1;
+  tmp = value;
+  for (;;) {
+    tmp /= 10;
+    if (0 >= tmp) {
+      break;
+    }
+    i++;
+  }
+  if (i >= max) {
+    buf[0] = '?';
+    buf[1] = 0x0;
+    return 2;
+  }
 
-	n = i;
-	tmp = value;
-	buf[i--] = 0x0;
-	for (;;) {
-		buf[i--] = (tmp % 10) + '0';
-		tmp /= 10;
-		if (0 >= tmp) {
-			break;
-		}
-	}
-	if (-1 != i) {
-		buf[i--] = '-';
-	}
-	return n;
+  n = i;
+  tmp = value;
+  buf[i--] = 0x0;
+  for (;;) {
+    buf[i--] = (tmp % 10) + '0';
+    tmp /= 10;
+    if (0 >= tmp) {
+      break;
+    }
+  }
+  if (-1 != i) {
+    buf[i--] = '-';
+  }
+  return n;
 }
 
 static WORKING_AREA(waThread2, 2048);
@@ -78,63 +77,63 @@ static msg_t Thread2(void *arg)  {
   (void)arg;
   chRegSetThreadName("lcd");
 
-	/* Reset */
+  /* Reset */
   palClearPad(GPIOC, GPIOC_PIN6);
   chThdSleepMilliseconds(10);
   palSetPad(GPIOC, GPIOC_PIN6);
   chThdSleepMilliseconds(10);
-	gdispInit();
-	gdispClear(Black);
+    gfxInit();
+  gdispClear(Black);
 
-	uint16_t width = gdispGetWidth();
-	uint16_t height = gdispGetHeight();
+  uint16_t width = gdispGetWidth();
+  uint16_t height = gdispGetHeight();
 
-	uint32_t pixels;
-	uint32_t i;
-	color_t random_color;
-	uint16_t rx, ry, rcx, rcy;
-	char pps_str[25];
-	srand(halGetCounterValue());
+  uint32_t pixels;
+  uint32_t i;
+  color_t random_color;
+  uint16_t rx, ry, rcx, rcy;
+  char pps_str[25];
+  srand(halGetCounterValue());
 
-	const font_t font1 = gdispOpenFont("UI2 Double");
+  const font_t font1 = gdispOpenFont("UI2 Double");
 
-	while (TRUE) {
+  while (TRUE) {
 
-	  gdispFillArea(10, 10, width-20, height-20, Grey);
-	  gdispFillArea(30, 30, 300, 150, Red);
-	  gdispFillArea(50, 50, 200, 100, Blue);
-	  gdispFillArea(80, 80, 150, 50, Yellow);
-	  gdispFillCircle(width/2, height/2, 50, White);
+    gdispFillArea(10, 10, width-20, height-20, Grey);
+    gdispFillArea(30, 30, 300, 150, Red);
+    gdispFillArea(50, 50, 200, 100, Blue);
+    gdispFillArea(80, 80, 150, 50, Yellow);
+    gdispFillCircle(width/2, height/2, 50, White);
 
-	  const char *msg = "ChibiOS/GFX on SSD1963";
-	  gdispDrawString(width-gdispGetStringWidth(msg, font1)-3, height-24, msg, font1, White);
+    const char *msg = "ChibiOS/GFX on SSD1963";
+    gdispDrawString(width-gdispGetStringWidth(msg, font1)-3, height-24, msg, font1, White);
 
-	  chThdSleepMilliseconds(2000);
-	  pixels = 0;
-	  gdispClear(Black);
-	  gdispDrawString(60, height/2, "Doing 5000 random rectangles", font1, White);
-	  chThdSleepMilliseconds(2000);
-	  uint32_t start = halGetCounterValue();
-	  for (i = 0; i < 5000; i++) {
-		  random_color = (rand() % 65535);
-		  rx = (rand() % (width-10));
-		  ry = (rand() % (height-10));
-		  rcx = (rand() % ((width-rx)-10))+10;
-		  rcy = (rand() % ((height-ry)-10))+10;
+    chThdSleepMilliseconds(2000);
+    pixels = 0;
+    gdispClear(Black);
+    gdispDrawString(60, height/2, "Doing 5000 random rectangles", font1, White);
+    chThdSleepMilliseconds(2000);
+    uint32_t start = halGetCounterValue();
+    for (i = 0; i < 5000; i++) {
+      random_color = (rand() % 65535);
+      rx = (rand() % (width-10));
+      ry = (rand() % (height-10));
+      rcx = (rand() % ((width-rx)-10))+10;
+      rcy = (rand() % ((height-ry)-10))+10;
 
-		  gdispFillArea(rx, ry, rcx, rcy, random_color);
-		  pixels += (rcx+1)*(rcy+1);
-	  }
-	  uint32_t ms = (halGetCounterValue()-start) / 168000;
-	  uint32_t pps = (float)pixels/((float)ms/1000.0f);
+      gdispFillArea(rx, ry, rcx, rcy, random_color);
+      pixels += (rcx+1)*(rcy+1);
+    }
+    uint32_t ms = (halGetCounterValue()-start) / 168000;
+    uint32_t pps = (float)pixels/((float)ms/1000.0f);
 
-	  memset (pps_str, 0, sizeof(pps_str));
-	  uitoa(pps, pps_str, sizeof(pps_str));
-	  strcat(pps_str, " Pixels/s");
+    memset (pps_str, 0, sizeof(pps_str));
+    uitoa(pps, pps_str, sizeof(pps_str));
+    strcat(pps_str, " Pixels/s");
 
-	  gdispClear(Black);
-	  gdispDrawString(100, height/2, pps_str, font1, White);
-	  chThdSleepMilliseconds(3000);
+    gdispClear(Black);
+    gdispDrawString(100, height/2, pps_str, font1, White);
+    chThdSleepMilliseconds(3000);
   }
 }
 
